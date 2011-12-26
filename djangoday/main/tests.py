aa63018882d91ae2dfcc3f645c28fea57f00e6a1
Sammyrulez <sammyrulez@gmail.com>
2011-12-26 13:57:25 +0100

    primo rilascio con test

--- djangoday/main/tests.py
+++ djangoday/main/tests.py
@@ -4,13 +4,33 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from django.core import urlresolvers
 
 from django.test import TestCase
+from django.test.client import Client
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_homepage(self):
+         c = Client()
+         response = c.get(urlresolvers.reverse('home'))
+         self.assertTrue(200,response.status_code)
+    def test_newsletter_submission(self):
+        c = Client()
+        response = c.post(urlresolvers.reverse('subscribe'))
+        self.assertTrue(200,response.status_code)
+
+        response = c.post(urlresolvers.reverse('subscribe'), {'inp_signup_email':'fail',})
+        self.assertTrue(200,response.status_code)
+
+
+
+        self.assertTrue(response.content.count('errorlist') > 0)
+        response = c.get(urlresolvers.reverse('subscribe'))
+        self.assertTrue(200,response.status_code)
+        self.assertTrue(response.content.count('errorlist') == 0)
+
+
+
+
+
